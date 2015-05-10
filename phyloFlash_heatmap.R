3cb710ff169c992c30717bf50c45e82486a0f054
Elmar Pruesse <elmar@pruesse.net>
2015-05-10 04:50:43 +0200

    let white in heatmap be "1", not min(matrix)

--- phyloFlash_heatmap.R
+++ phyloFlash_heatmap.R
@@ -189,6 +189,7 @@ g_make_heatmap <- function(mat, n, angle=90, hjust=0,vjust=0.6) {
        geom_tile(aes(fill=value)) +
        scale_fill_gradient(low="white", high=highcol, trans="log",
                            breaks=breaks,
+                           limits=c(1,max(mat$value)),
                            na.value="white") +
        labs(x = NULL, y = NULL) +
        scale_x_discrete(expand=c(0,0)) +
