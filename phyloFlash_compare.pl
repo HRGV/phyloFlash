a118ca898e12fb09596a24c8a5daa99d39ecd62e
kbseah <kbseah@users.noreply.github.com>
2018-05-02 15:16:23 +0200

    Add copyright statement

--- phyloFlash_compare.pl
+++ phyloFlash_compare.pl
@@ -854,3 +854,26 @@ sub welcome {
     print STDERR $message;
     print STDERR "\n";
 }
+
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2018- Brandon Seah <kbseah@mpi-bremen.de> with
+                    Harald Gruber-Vodicka <hgruber@mpi-bremen.de>
+                    and Elmar A. Pruesse <elmar.pruesse@ucdenver.edu>
+
+LICENCE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=cut
