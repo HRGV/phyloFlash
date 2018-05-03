626430ddd2436ee2cde19c47674ec7b76d32f4ba
kbseah <kbseah@users.noreply.github.com>
2018-05-03 13:29:59 +0200

    Add specify download dir for ENA read libs

--- ENA_phyloFlash.pl
+++ ENA_phyloFlash.pl
@@ -15,7 +15,7 @@
 Download Fastq files associated with an ENA Read Archive run accession number,
 and run phyloFlash on those files if option -phyloFlash is chosen.
 
-Requires phyloFlash v3.0+
+Requires phyloFlash v3.0+ and wget
 
 =cut
 
@@ -30,11 +30,14 @@ use lib $FindBin::RealBin;
 use Getopt::Long;
 use POSIX;
 use Pod::Usage;
+use PhyloFlash qw(msg err @msg_log $VERSION);
 
 # Input params
 my $acc;
 my $CPUs = 8; # Num processors - for nhmmer
 my $download = 1; # By default, download
+my $download_dir = ".";
+my $cleanup;
 my $run_pF = 0; # By default, do not run phyloFlash
 my $pF_dbhome;
 my $http_proxy;
@@ -44,51 +47,72 @@ pod2usage(2) if (!@ARGV);
 # Get input from command line
 GetOptions ("acc=s" => \$acc,
             "CPUs=i" => \$CPUs,
-            "download!" => \$download,
-            "phyloFlash!" => \$run_pF,
+            "download|d!" => \$download,
+            "download_dir=s" => \$download_dir,
+            "cleanup" => \$cleanup,
+            "phyloFlash|p!" => \$run_pF,
             "dbhome=s" => \$pF_dbhome,
             "http_proxy=s" => \$http_proxy,
-            'help' => sub { pod2usage(1) },
-            'man' => sub { pod2usage(-exitval=>0, -verbose=>2) },
+            'help|h' => sub { pod2usage(1) },
+            'man' => sub { pod2usage(-exit=>0, -verbose=>2) },
+            'version|v'  => sub { welcome(); exit(); },
             ) or pod2usage(2);
 
-=head1 INPUT ARGUMENTS
+=head1 ARGUMENTS
 
 =over 8
 
-=item -acc STRING
+=item --acc STRING
 
-Accession number of read run
+Accession number of read run. Please ensure that this is the I<Run> accession
+and not e.g. sample or study accession number. They are typically prefixed by
+ERR..., SRR..., or DRR.... 
 
-=item -CPUs INTEGER
+=item --CPUs I<INTEGER>
 
 Number of processors to use (passed to phyloFlash) (Default: 8)
 
-=item -download
+=item -d|--download
 
 Download files? Turn off with -nodownload (Default: Yes)
 
-=item -phyloFlash
+=item --download_dir I<PATH>
+
+Path to download files.
+
+Default: Current folder
+
+=item --cleanup
+
+Delete downloaded read files
+
+Default: No
+
+=item -p|--phyloFlash 
 
 Run phyloFlash? (Default: No, download only)
 
-=item -dbhome PATH
+=item --dbhome I<PATH>
 
 Path to phyloFlash database, otherwise use default, which is to look in folder
 where phyloFlash script is located
 
-=item -http_proxy URL
+=item --http_proxy I<URL>
 
 URL for http proxy - be sure to specify the protocol with "http://..."
 (Default: none)
 
-=item -help
+=item --help
 
 Show help message
 
-=item -man
+=item --man
 
 Show manual page in pager
+
+=item --version
+
+Print version number
 
 =back
 
@@ -97,6 +121,7 @@ Show manual page in pager
 # Variables
 my @fastq_urls; # List of Fastq URLs
 my @fastq_basenames;
+my @fastq_fullpaths;
 my $phyloFlash = "$FindBin::RealBin/phyloFlash.pl"; # assume phyloFlash script in same folder
 my $wget = "wget --no-verbose";   # Wget binary and options
 
@@ -109,6 +134,16 @@ if (@fastq_urls) {
     say STDERR join ("\n", @fastq_urls);
 }
 
+# Check download path to make sure is a directory and is writable
+if (defined $download_dir) {
+    if (! -d $download_dir) {
+        die ("Path to place downloaded files $download_dir is not a valid directory");
+    } elsif (! -w $download_dir) {
+        die ("You do not have write permissions to path $download_dir");
+    } else {
+        $wget .= " -P $download_dir"; # Add path option to wget command
+    }
+}
 
 # Check if any URLs were retrieved
 if (defined $fastq_urls[0]) {
@@ -122,7 +157,7 @@ if (defined $fastq_urls[0]) {
     }
 }
 
-# Run phyloFlash (v3.0beta1) to extract SSU reads
+# Run phyloFlash (v3.0beta1+) to extract SSU reads
 my @pF_args = ("-lib pF_$acc",
                "-CPUs $CPUs",
                "-emirge",               # Run both SPAdes and EMIRGE
@@ -133,17 +168,31 @@ my @pF_args = ("-lib pF_$acc",
 # If specific dbhome specified, pass to phyloFlash, otherwise use default detected
 push @pF_args, "-dbhome $pF_dbhome" if defined $pF_dbhome;
 
+# Add paths to Fastq file names
+
+foreach my $file (@fastq_basenames) {
+    push @fastq_fullpaths, "$download_dir/$file";
+}
+
 # Check how many Fastq files (paired or unpaired)
-if (scalar (@fastq_basenames) == 1) {
-    push @pF_args, "-read1 ".$fastq_basenames[0];
+if (scalar (@fastq_fullpaths) == 1) {
+    push @pF_args, "-read1 ".$fastq_fullpaths[0];
 } elsif (scalar (@fastq_basenames) == 2) {
-    push @pF_args, "-read1 ".$fastq_basenames[0];
-    push @pF_args, "-read2 ".$fastq_basenames[1];
+    push @pF_args, "-read1 ".$fastq_fullpaths[0];
+    push @pF_args, "-read2 ".$fastq_fullpaths[1];
 }
 
 system (join " ", ($phyloFlash, @pF_args)) if $run_pF == 1;
 
+unlink @fastq_fullpaths if defined $cleanup; # Delete read files 
+
 ## SUBS ########################################################################
+
+sub welcome {
+    my $message = "This is ENA_phyloFlash.pl from phyloFlash v$VERSION";
+    print STDERR $message;
+    print STDERR "\n";
+}
 
 
 sub get_fastq_urls {
